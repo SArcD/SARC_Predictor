d4676177504b21d7b1b4e4bb7fe926ef0ec44ba0
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 19:14:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Ruta del archivo de Excel
 file_path = "Base 2019 Santiago Arceo.xlsx"
@@ -13,22 +12,6 @@ try:
     datos = pd.read_excel(file_path, engine='openpyxl')
     st.write("Datos cargados con éxito:")
     st.dataframe(datos)
-
-    # Reemplazar valores numéricos con etiquetas 'Hombre' y 'Mujer' en la columna 'sexo'
-    datos['sexo'] = datos['sexo'].replace({1.0: 'Hombre', 2.0: 'Mujer'})
-
-    # Generar gráfico de pastel para la columna 'sexo'
-    sexo_counts = datos['sexo'].value_counts()
-
-    # Crear la figura y mostrarla en Streamlit
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.pie(sexo_counts, labels=sexo_counts.index, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightpink'])
-    ax.set_title('Proporción de Hombres vs Mujeres')
-    ax.axis('equal')  # Asegurar que el gráfico sea un círculo.
-
-    # Mostrar el gráfico en Streamlit
-    st.pyplot(fig)
-
 except FileNotFoundError:
     st.error("No se encontró el archivo en la ruta especificada.")
 except Exception as e:
