674b45e76caf5a82234396d483f128484ff30606
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 18:15:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Ruta al archivo xlsx
-archivo_xlsx = "/content/Base 2019 Santiago Arceo.xlsx"
+archivo_xlsx = "Base 2019 Santiago Arceo.xlsx"
 
 datos = pd.read_excel(archivo_xlsx)
 
