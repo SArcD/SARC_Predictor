5e531f96d3538ba6a83c965b3939cdba5b622e0f
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 18:47:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-# Ruta del archivo de Excel
-file_path = "Base 2019 Santiago Arceo.xlsx"
-
 # Título de la app
 st.title("Sarc_Predictor: predecir sarcopenia mediante Machine-Learning")
 
+# Subir archivo de Excel
+uploaded_file = st.file_uploader("Cargar archivo de Excel", type="xlsx")
+
 # Leer y mostrar el archivo de Excel
-try:
-    datos = pd.read_excel(file_path, engine='openpyxl')
-    st.write("Datos cargados con éxito:")
-    st.dataframe(datos)
-except FileNotFoundError:
-    st.error("No se encontró el archivo en la ruta especificada.")
-except Exception as e:
-    st.error(f"Error al cargar el archivo: {e}")
+if uploaded_file is not None:
+    try:
+        datos = pd.read_excel(uploaded_file)
+        st.write("Datos cargados con éxito:")
+        st.dataframe(datos)
+    except Exception as e:
+        st.error(f"Error al cargar el archivo: {e}")
+else:
+    st.info("Por favor, sube un archivo de Excel.")
