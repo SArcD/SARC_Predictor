8c4f5b066ccd7e53b46ae62beee0fa46fa45b108
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 18:43:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pandas as pd
+import requests
+from io import BytesIO
 
 # Título de la app
 st.title("Sarc_Predictor: predecir sarcopenia mediante Machine-Learning")
 
 # URL del archivo de Excel en formato raw
-file_url = "https://raw.githubusercontent.com/SArcD/SARC_Predictor/main/Base%202019%20Santiago%20Arceo.xlsx"
+file_url = "https://raw.githubusercontent.com/SArcD/SARC_Predictor/dc0862d02d4cc1187e1b8dd5a991427132cd55c1/Base%202019%20Santiago%20Arceo.xlsx"
 
-# Leer y mostrar el archivo de Excel especificando el motor 'openpyxl'
+# Descargar el archivo y cargarlo en un DataFrame
 try:
-    datos = pd.read_excel(file_url, engine='openpyxl')
+    response = requests.get(file_url)
+    response.raise_for_status()  # Verificar si la descarga fue exitosa
+    datos = pd.read_excel(BytesIO(response.content), engine='openpyxl')
     st.write("Datos cargados con éxito:")
     st.dataframe(datos)
 except Exception as e:
