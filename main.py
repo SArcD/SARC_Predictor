c6317a8500a9485e07653691bd9461cad45215ab
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 18:22:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import streamlit as st
 import pandas as pd
 
-# Ruta al archivo xlsx
-archivo_xlsx = "Base 2019 Santiago Arceo.xlsx"
+# Cargar archivo de Excel
+file_path = "Base 2019 Santiago Arceo.xlsx"
 
-datos = pd.read_excel(archivo_xlsx)
+# Título de la app
+st.title("Sarc_Predictor: predecir sarcopenia mediante Machine-Learning")
 
-datos
+# Leer y mostrar el archivo de Excel
+try:
+    datos = pd.read_excel(file_path)
+    st.write("Datos cargados con éxito:")
+    st.dataframe(data)
+except FileNotFoundError:
+    st.error("No se encontró el archivo en la ruta especificada.")
