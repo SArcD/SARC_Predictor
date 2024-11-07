cd4ab883c5261f26e512fd4ee99beb361fcb295a
SArcD <141703135+SArcD@users.noreply.github.com>
2024-11-06 18:44:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import streamlit as st
 import pandas as pd
-import requests
-from io import BytesIO
+
+# Ruta del archivo de Excel
+file_path = "Base 2019 Santiago Arceo.xlsx"
 
 # Título de la app
 st.title("Sarc_Predictor: predecir sarcopenia mediante Machine-Learning")
 
-# URL del archivo de Excel en formato raw
-file_url = "https://raw.githubusercontent.com/SArcD/SARC_Predictor/dc0862d02d4cc1187e1b8dd5a991427132cd55c1/Base%202019%20Santiago%20Arceo.xlsx"
-
-# Descargar el archivo y cargarlo en un DataFrame
+# Leer y mostrar el archivo de Excel
 try:
-    response = requests.get(file_url)
-    response.raise_for_status()  # Verificar si la descarga fue exitosa
-    datos = pd.read_excel(BytesIO(response.content), engine='openpyxl')
+    datos = pd.read_excel(file_path, engine='openpyxl')
     st.write("Datos cargados con éxito:")
     st.dataframe(datos)
+except FileNotFoundError:
+    st.error("No se encontró el archivo en la ruta especificada.")
 except Exception as e:
     st.error(f"Error al cargar el archivo: {e}")
