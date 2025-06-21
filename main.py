ae1d61d97707bb9e4bc5f6f0844b7fd24fec39f9
SArcD <141703135+SArcD@users.noreply.github.com>
2025-06-20 21:20:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -645,7 +645,7 @@ Para garantizar que los modelos predictivos se entrenen únicamente con variable
             import plotly.subplots as sp
             st.markdown("""
 <div style='text-align: justify'>
-La <strong>siguiente figura</strong> muestra los <strong>histogramas</strong> con la distribución de valores para cada uno de los parámetros.  
+La <strong>Figura 5</strong> muestra los <strong>histogramas</strong> con la distribución de valores para cada uno de los parámetros.  
 La muestra original se ha separado en los subconjuntos de hombres y mujeres.  
 
 En algunos casos, los dos subconjuntos presentan distribuciones similares 
@@ -772,7 +772,18 @@ En otros casos, cada histograma muestra picos en zonas diferentes, lo que reflej
             from sklearn.preprocessing import MinMaxScaler
             import numpy as np
             import pandas as pd
-            st.markdown("""El siguiente paso en la simplificación consitió en buscar aquellas variables que pudieran estar muy correlacionadas en tre sí ya que si dos variables lo están entonces solo necesitamos conservar una para incluirla en los modelos. La siguiente figura muestra la matriz de correlación entre las variables. La intensidad de los tonos azules o rojos representan la magnitud de la correlacción. **Para nuestra lista final de variables conservamos solo variables que no estuvieran correlacionadas entre si con mas de 0.70 de correlación**.""")
+            st.markdown("""
+<div style='text-align: justify'>
+El siguiente paso en la simplificación consistió en identificar aquellas variables que pudieran estar altamente correlacionadas entre sí.  
+Cuando dos variables muestran una correlación elevada, es suficiente conservar solo una de ellas para entrenar los modelos.  
+
+La siguiente figura muestra la matriz de correlación entre las variables.  
+La intensidad de los tonos azules o rojos representa la magnitud de la correlación.  
+
+<strong>Para nuestra lista final de variables, conservamos solo aquellas que no estuvieran correlacionadas entre sí con más de 0.70 de correlación.</strong>
+</div>
+""", unsafe_allow_html=True)
+
             # --- Columnas seleccionadas ---
             selected_columns = ['P112_vel','P113', 'P125', 'P128', 'P127','P126','IMC','P121','P123','P120', 'P124']
 
